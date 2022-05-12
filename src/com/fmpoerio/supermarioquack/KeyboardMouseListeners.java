34ef99caa0edcb14f0b1e086964f7c727737bfd3
fracchiomon <poerio.francescomaria@gmail.com>
2022-05-12 15:18:08 +0200

    Per ora Funziona, ho iniziato ad implementare il KeyMouse Listener

---
+++ src/com/fmpoerio/supermarioquack/KeyboardMouseListeners.java
@@ -0,0 +1,61 @@
+package com.fmpoerio.supermarioquack;
+
+import java.awt.event.*;
+
+public class KeyboardMouseListeners implements KeyListener, MouseListener, MouseMotionListener {
+
+    public static final int[] MOVE_KEYS = {KeyEvent.VK_W, KeyEvent.VK_A, KeyEvent.VK_S, KeyEvent.VK_D,
+                                        KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT,
+                                        KeyEvent.VK_KP_UP,KeyEvent.VK_KP_DOWN,KeyEvent.VK_KP_LEFT,KeyEvent.VK_KP_RIGHT};
+    public boolean click = false;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
--- src/com/fmpoerio/supermarioquack/MenuState.java
+++ src/com/fmpoerio/supermarioquack/MenuState.java
@@ -7,12 +7,14 @@ import java.net.URL;
 public class MenuState extends GameState{
 
     protected Image background;
+    protected final Color bgColor = Color.BLACK;
 
     protected static final String[] OPTIONS = {"Nuova Partita", "Help", "Esci"};
     private int currentSelection = 0;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
+
         background = new ImageIcon("Assets/SuperMarioQuack_MarioQuack.png").getImage();
 
     }
@@ -25,13 +27,15 @@ public class MenuState extends GameState{
 
     @Override
     public void draw(Graphics g) {
+        g.setColor(bgColor);
+        g.fillRect(0,0,GamePanel.getWIDTH(), GamePanel.getHEIGHT());
         g.drawImage(background,GamePanel.getWIDTH()/2 + 100, GamePanel.getHEIGHT()/2 - 100, null);
         for(int i = 0; i < OPTIONS.length; i++) {
             if(i == currentSelection) {
                 g.setColor(Color.YELLOW);
             }
             else {
-                g.setColor(Color.BLACK);
+                g.setColor(Color.WHITE);
             }
             g.setFont(new Font(QuackMario.getFONT(), Font.PLAIN, 48));
             g.drawString(OPTIONS[i], GamePanel.getWIDTH()/2 - 400, 300 + i * 75 );
